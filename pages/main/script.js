47a331e0d32bf0cf64fad140b944f3a57c5435e0
DmitryBelotsky <belotsky.dmitry@gmail.com>
2022-04-28 12:05:07 +0300

    init

---
+++ pages/main/script.js
@@ -0,0 +1,8 @@
+const BTN_LEFT = document.querySelector("#btn-left");
+const BTN_RIGHT = document.querySelector("#btn-right");
+
+const CAROUSEL = document.querySelector("#carousel");
+
+BTN_LEFT.addEventListener("click", function() {
+    CAROUSEL
+});
